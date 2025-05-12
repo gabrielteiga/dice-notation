e4efe88bb143c3ccfb235be89c4ec47f46ca2bbc
Gabriel Teiga <gabrielteiga99@gmail.com>
2025-05-12 15:08:58 -0300

    feat: creating the calculate logic from dices

--- src/main/java/trabalho/antlr/dice/app/Main.java
+++ src/main/java/trabalho/antlr/dice/app/Main.java
@@ -4,6 +4,7 @@ import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import trabalho.antlr.dice.lexer.DiceNotationLexer;
 import trabalho.antlr.dice.parser.DiceNotationParser;
+import trabalho.antlr.dice.parser.visitor.DiceEvaluator;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -13,7 +14,12 @@ public class Main {
         DiceNotationParser parser = new DiceNotationParser(tokens);
 
         ParseTree tree = parser.file_();
+
+        DiceEvaluator dice = new DiceEvaluator();
+        int resultado = dice.visit(tree);
+
         System.out.println(tree.getText());
         System.out.println(tree.toStringTree(parser));
+        System.out.println(resultado);
     }
 }
---
+++ src/main/java/trabalho/antlr/dice/parser/visitor/DiceEvaluator.java
@@ -0,0 +1,58 @@
+package trabalho.antlr.dice.parser.visitor;
+
+import trabalho.antlr.dice.parser.DiceNotationParser;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DiceEvaluator extends DiceNotationParserBaseVisitor<Integer> {
+    @Override
+    public Integer visitFile_(DiceNotationParser.File_Context ctx) {
+        return visit(ctx.notation());
+    }
+
+    @Override
+    public Integer visitAddOp(DiceNotationParser.AddOpContext ctx) {
+        int acc = visit(ctx.multOp(0));
+        for (int i = 1; i < ctx.multOp().size(); i++) {
+            String op   = ctx.ADDOPERATOR(i - 1).getText();
+            int rhs     = visit(ctx.multOp(i));
+            acc         = op.equals("+") ? acc + rhs : acc - rhs;
+            System.out.println(op);
+            System.out.println(rhs);
+            System.out.println(acc);
+        }
+        return acc;
+    }
+
+    @Override
+    public Integer visitOperand(DiceNotationParser.OperandContext ctx) {
+        if(ctx.number() != null)    return visit(ctx.number());
+        if(ctx.dice() != null)      return visit(ctx.dice());
+        return visit(ctx.notation());
+    }
+
+    @Override
+    public Integer visitNumber(DiceNotationParser.NumberContext ctx) {
+        return Integer.parseInt(ctx.getText());
+    }
+
+    @Override
+    public Integer visitDice(DiceNotationParser.DiceContext ctx) {
+        String txt = ctx.getText();
+        boolean neg = txt.startsWith("-");
+
+        txt = txt.replaceFirst("^[+-]", "");
+
+        String[] parts = txt.split("[dD]");
+        int count = parts[0].isEmpty() ? 1 : Integer.parseInt(parts[0]);
+        int sides = Integer.parseInt(parts[1]);
+
+        int sum = 0;
+        for (int i = 0; i < count; i++) {
+            sum += ThreadLocalRandom
+                    .current()
+                    .nextInt(1, sides + 1);
+        }
+        return neg ? -sum : sum;
+    }
+}
